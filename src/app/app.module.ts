9e0ccff609cd49a8a5058a30042b8f9c7b16f3cc
Jonathas Germano de Oliveira <estagio.jonathas@jfpb.jus.br>
2018-08-08 11:45:26 -0300

    Preparo base roteamento, reconfiguração de componentes base.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
-import { LoginComponent } from './login/login.component';
+import { StatsComponent } from './stats/stats.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,

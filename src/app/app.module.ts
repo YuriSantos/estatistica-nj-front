26c49a5bd741b535f2345eb9d8dde90b768b1bb5
Jonathas Germano de Oliveira <estagio.jonathas@jfpb.jus.br>
2018-08-15 11:15:55 -0300

    Mudanças de cabeçalho de pesquisa de componente stats para app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +21,7 @@ import { StatsComponent } from './stats/stats.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.ts
+++ src/app/material.ts
@@ -3,13 +3,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 import {MatSelectModule} from '@angular/material/select';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule, MatSelectModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule, MatSelectModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule, MatSelectModule, MatDividerModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule, MatSelectModule, MatDividerModule],
 })
 export class MaterialModule { }

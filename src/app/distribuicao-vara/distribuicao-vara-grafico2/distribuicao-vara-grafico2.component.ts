b570c85f74f81eaeacb4b16668fe35c9bfd9b026
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2018-09-12 21:44:23 -0300

    Adicionado o src-service + observable na distribuicao-vara-grafico2

--- src/app/distribuicao-vara/distribuicao-vara-grafico2/distribuicao-vara-grafico2.component.ts
+++ src/app/distribuicao-vara/distribuicao-vara-grafico2/distribuicao-vara-grafico2.component.ts
@@ -4,6 +4,7 @@ import {DistribuicaoVara} from '../../models/distribuicao-vara.model';
 import {Router} from '@angular/router';
 import {DistribuicaoVaraService} from '../../services/distribuicao-vara.service';
 import {ResponseApi} from '../../models/response-api';
+import {SrcBarService} from '../../services/src-bar.service';
 
 @Component({
   selector: 'app-distribuicao-vara-grafico2',
@@ -17,18 +18,50 @@ export class DistribuicaoVaraGrafico2Component implements OnInit {
   dataGrafico = [];
   displayedColumns: string[] = ['Distribuidos', 'Arquivados', 'Total'] ;
   Tabela = [];
+  ano;
+  mes;
 
   constructor(private router: Router,
-              private distribuicaoVaraService: DistribuicaoVaraService) {
+              private distribuicaoVaraService: DistribuicaoVaraService,
+              private srcBarService: SrcBarService) {
     this.shared = SharedService.getInstance();
   }
 
   ngOnInit() {
-    this.findAno(2018);
+    const date = new Date();
+    this.findAno(date.getFullYear());
+    this.srcBarService.anoDisparado.subscribe(
+      (ano: number) => {
+        this.ano = ano;
+      }
+    );
+    this.srcBarService.mesDisparado.subscribe(
+      (mes: number) => {
+        this.mes = mes;
+        if (mes !== null && mes !== undefined) {
+          this.findMesAno(this.ano, this.mes);
+        } else {
+          this.findAno(this.ano);
+        }
+      }
+    );
   }
 
   findAno(ano: number) {
     this.distribuicaoVaraService.findByAno(ano)
+      .subscribe((responseApi: ResponseApi) => {
+        this.listDistVara = responseApi.data;
+        this.distVara = responseApi['data'];
+        this.dataGrafico = [this.listDistVara.eletronicoArquivado, this.listDistVara.eletronicoDistribuido];
+        this.Tabela = [{
+          eletronicoArquivado: this.listDistVara.eletronicoArquivado, eletronicoDistribuido: this.listDistVara.eletronicoDistribuido,
+          total: (this.listDistVara.eletronicoArquivado + this.listDistVara.eletronicoDistribuido)
+        }];
+      });
+  }
+
+  findMesAno(ano: number, mes: number) {
+    this.distribuicaoVaraService.findByMes(ano, mes)
       .subscribe((responseApi: ResponseApi) => {
         this.listDistVara = responseApi.data;
         this.distVara = responseApi['data'];
@@ -44,12 +77,14 @@ export class DistribuicaoVaraGrafico2Component implements OnInit {
   public pieChartLabels: string[] = ['Acordo', 'Sem Acordo'];
   // tslint:disable-next-line:member-ordering
   public pieChartData: number[] = [300, 500];
+  // tslint:disable-next-line:member-ordering
   public pieChartType = 'pie';
+  // tslint:disable-next-line:member-ordering
   public coresGrafico: Array<any> = [
     { // first color
       backgroundColor: ['#00a7e1', '#003459', '#FAC05E']
     }
-  ]
+  ];
 
   // events
   public chartClicked(e: any): void {

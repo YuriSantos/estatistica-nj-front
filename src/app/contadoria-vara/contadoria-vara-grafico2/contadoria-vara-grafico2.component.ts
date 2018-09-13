4b6f71c916b05c9dddee589e959bbfa34befcbe1
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2018-09-12 21:33:49 -0300

    Adicionado o src-service + observable na contadoria-vara-grafico2

--- src/app/contadoria-vara/contadoria-vara-grafico2/contadoria-vara-grafico2.component.ts
+++ src/app/contadoria-vara/contadoria-vara-grafico2/contadoria-vara-grafico2.component.ts
@@ -4,6 +4,7 @@ import {ContadoriaVara} from '../../models/contadiria-vara.model';
 import {Router} from '@angular/router';
 import {ContadoriaVaraService} from '../../services/contadoria-vara.service';
 import {ResponseApi} from '../../models/response-api';
+import {SrcBarService} from '../../services/src-bar.service';
 
 @Component({
   selector: 'app-contadoria-vara-grafico2',
@@ -17,18 +18,51 @@ export class ContadoriaVaraGrafico2Component implements OnInit {
   dataGrafico = [];
   displayedColumns: string[] = ['Físico Saída', 'Eletrônico Saída', 'Total'];
   Tabela = [];
+  ano;
+  mes;
 
   constructor(private router: Router,
-              private contVaraService: ContadoriaVaraService) {
+              private contVaraService: ContadoriaVaraService,
+              private srcBarService: SrcBarService) {
     this.shared = SharedService.getInstance();
   }
 
   ngOnInit() {
-    this.findAno(2018);
+    const date = new Date();
+    this.findAno(date.getFullYear());
+    this.srcBarService.anoDisparado.subscribe(
+      (ano: number) => {
+        this.ano = ano;
+      }
+    );
+    this.srcBarService.mesDisparado.subscribe(
+      (mes: number) => {
+        this.mes = mes;
+        if (mes !== null && mes !== undefined) {
+          this.findMesAno(this.ano, this.mes);
+        } else {
+          this.findAno(this.ano);
+        }
+      }
+    );
   }
 
   findAno(ano: number) {
     this.contVaraService.findByAno(ano)
+      .subscribe((responseApi: ResponseApi) => {
+        this.listContVara = responseApi.data;
+        this.contVara = responseApi['data'];
+        this.dataGrafico = [this.listContVara.fisicoSaida, this.listContVara.eletronicoSaida];
+        this.Tabela = [{
+          fisicoSaida: this.listContVara.fisicoSaida, eletronicoSaida: this.listContVara.eletronicoSaida,
+          total: (this.listContVara.fisicoSaida + this.listContVara.eletronicoSaida)
+        }];
+
+      });
+  }
+
+  findMesAno(ano: number, mes: number ) {
+    this.contVaraService.findByMes(ano, mes)
       .subscribe((responseApi: ResponseApi) => {
         this.listContVara = responseApi.data;
         this.contVara = responseApi['data'];
@@ -46,12 +80,14 @@ export class ContadoriaVaraGrafico2Component implements OnInit {
   public pieChartLabels: string[] = ['Físico Saída', 'Eletrônico Saída'];
   // tslint:disable-next-line:member-ordering
   public pieChartData: number[] = [300, 500];
+  // tslint:disable-next-line:member-ordering
   public pieChartType = 'pie';
+  // tslint:disable-next-line:member-ordering
   public coresGrafico: Array<any> = [
     { // first color
       backgroundColor: ['#00a7e1', '#003459', '#FAC05E']
     }
-  ]
+  ];
 
   // events
   public chartClicked(e: any): void {

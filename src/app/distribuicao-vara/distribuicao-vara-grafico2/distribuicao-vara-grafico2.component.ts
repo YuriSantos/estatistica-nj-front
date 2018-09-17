c1aa5333e78873bb0054bb90006495fcca442df6
Yuri Santos <yurisantosjp@gmail.com>
2018-09-17 12:58:35 -0300

    Colocando as alterações de Gildazio

--- src/app/distribuicao-vara/distribuicao-vara-grafico2/distribuicao-vara-grafico2.component.ts
+++ src/app/distribuicao-vara/distribuicao-vara-grafico2/distribuicao-vara-grafico2.component.ts
@@ -16,7 +16,7 @@ export class DistribuicaoVaraGrafico2Component implements OnInit {
   listDistVara: DistribuicaoVara;
   distVara: DistribuicaoVara[];
   dataGrafico = [];
-  displayedColumns: string[] = ['Distribuidos', 'Arquivados', 'Total'] ;
+  displayedColumns: string[] = ['Distribuidos', 'Arquivados', 'Migrados para o PJE', 'Total'] ;
   Tabela = [];
   ano;
   mes;
@@ -74,16 +74,18 @@ export class DistribuicaoVaraGrafico2Component implements OnInit {
             0, 0, 0, 0,
             0, 0, 0);
         }
-        this.dataGrafico = [this.listDistVara.eletronicoArquivado, this.listDistVara.eletronicoDistribuido];
+        this.dataGrafico = [this.listDistVara.eletronicoArquivado, this.listDistVara.eletronicoDistribuido,
+          this.listDistVara.processosMigrados];
         this.Tabela = [{
           eletronicoArquivado: this.listDistVara.eletronicoArquivado, eletronicoDistribuido: this.listDistVara.eletronicoDistribuido,
-          total: (this.listDistVara.eletronicoArquivado + this.listDistVara.eletronicoDistribuido)
+          processosMigrados: this.listDistVara.processosMigrados,
+          total: (this.listDistVara.eletronicoArquivado + this.listDistVara.eletronicoDistribuido + this.listDistVara.processosMigrados)
         }];
       });
   }
 // Pie
   // tslint:disable-next-line:member-ordering
-  public pieChartLabels: string[] = ['Acordo', 'Sem Acordo'];
+  public pieChartLabels: string[] = ['Eletronico Arquivado', 'Eletronico Distribuido', 'Processos Migrados para o PJE'];
   // tslint:disable-next-line:member-ordering
   public pieChartData: number[] = [300, 500];
   // tslint:disable-next-line:member-ordering
